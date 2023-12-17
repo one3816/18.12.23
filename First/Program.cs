e4945090829069ff296830bd03a0676b5de9c35c
one3816 <one3816@mail.ru>
2023-12-18 04:01:48 +0500

    commit

---
+++ First/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите начальное число");
+int start = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите конечное число");
+int finish = Convert.ToInt32(Console.ReadLine());
+void GetNumbers(int start,int finish)
+{
+    if(start <= finish)
+    {
+        Console.Write(start + " ");
+        GetNumbers(start+1,finish);
+    }
+}
+GetNumbers(start,finish);
---
+++ Third/Program.cs
@@ -0,0 +1,32 @@
+﻿int[] GetNewArray(int size)
+{
+    int[] newArray = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        newArray[i] = rnd.Next(1, 100);
+    }
+    return newArray;
+}
+
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+
+        Console.Write(arr[i] + " ");
+    }
+}
+int[] arr = GetNewArray(10);
+PrintArray(arr);
+Console.WriteLine();
+TurnOver(arr,0);
+
+void TurnOver(int[] array,int i)
+{
+    if(i<array.Length)
+    {
+       Console.Write(array[array.Length-1-i] + " ");
+       TurnOver(array,i+1);
+    } 
+}
